6935fcef3f53f0f7c9f322847082513d0c9daee4
Weronika Mirek <werussia16@gmail.com>
2019-07-09 17:14:46 +0200

    MatrixTransposer matrix validation test

--- test/MatrixTransposerTest.java
+++ test/MatrixTransposerTest.java
@@ -1,3 +1,4 @@
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -5,35 +6,47 @@ import java.io.*;
 
 import static org.junit.Assert.assertEquals;
 
-public class MatrixTransposerTest{
+public class MatrixTransposerTest {
 
     private MatrixTransposer matrixTransposer;
 
     @Before
     public void setUp() {
-        matrixTransposer=new MatrixTransposer();
+        matrixTransposer = new MatrixTransposer();
     }
 
     @Test
-    public void transpose() throws IOException {
-        final String inputFileName = "testInputFile.txt";
-        final String outputFileName = "testOutputFile.txt";
+    public void transpose_success() throws IOException {
+        final String inputFileName = "successTestInputFile.txt";
+        final String outputFileName = "successTestOutputFile.txt";
         FileWriter fileWriter = new FileWriter(inputFileName);
-        fileWriter.write("1 2 3 \n4 5 6 \n7 8 9 ");
+        fileWriter.write("1 2 3 \n4 5 6 \n7 8 9 \n");
         fileWriter.close();
 
-        matrixTransposer.transpose(inputFileName,outputFileName);
+        matrixTransposer.transpose(inputFileName, outputFileName);
 
         File file = new File(outputFileName);
         BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
         StringBuilder outputFileContent = new StringBuilder();
         String line = bufferedReader.readLine();
-        while(line != null){
+        while (line != null) {
             outputFileContent.append(line);
             outputFileContent.append("\n");
             line = bufferedReader.readLine();
         }
 
-        assertEquals("1 4 7 \n2 5 8 \n3 6 9 \n",outputFileContent.toString());
+        assertEquals("1 4 7 \n2 5 8 \n3 6 9 \n", outputFileContent.toString());
+
     }
+
+    @Test(expected = IOException.class)
+    public void transpose_matrixValidation() throws IOException {
+        final String inputFileName = "validationTestInputFile.txt";
+        FileWriter fileWriter = new FileWriter(inputFileName);
+        fileWriter.write("1 2 3 \n4 5 \n");
+        fileWriter.close();
+
+        matrixTransposer.transpose(inputFileName, "validationTestOutputFile.txt");
+    }
+
 }
